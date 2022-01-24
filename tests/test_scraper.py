86fed6c15b86b84c70ff70bccda718e2f4775ba7
giocaizzi <giocaizzi@gmail.com>
2022-01-24 23:42:15 +0100

    test scraper

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -3,13 +3,26 @@ test scraper module.
 """
 import pytest
 import pandas as pd
+import os
 
 from surfingcrypto.scraper import Scraper 
 from surfingcrypto.config import config
 
-
-@pytest.mark.skip
-def test_load_config(temp_test_env):
-    c=config(str(temp_test_env/"config"))
+@pytest.mark.parametrize('check_file',[False,True])
+@pytest.mark.parametrize(
+    'temp_test_env',
+    [("config.json",),],
+    indirect=True
+    )
+@pytest.mark.wip
+def test_load_config(
+    temp_test_env,
+    check_file
+    ):
+    root=temp_test_env
+    c=config(str(root/"config"))
     s=Scraper(c)
     assert isinstance(s.config,config)
+    if check_file:
+        s.run()
+        assert os.path.isfile(root/"data"/"ts"/"BTC.csv")
