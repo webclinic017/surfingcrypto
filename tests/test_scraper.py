96187e197d376b4f559d8d978d3adf7207a16f15
giocaizzi <giocaizzi@gmail.com>
2022-01-25 22:34:24 +0100

    scraper testing

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,6 +1,7 @@
 """
 test scraper module.
 """
+import datetime
 import pytest
 import pandas as pd
 import os
@@ -8,23 +9,36 @@ import os
 from surfingcrypto.scraper import Scraper 
 from surfingcrypto.config import config
 
-@pytest.mark.parametrize('run',[(False,True)])
-@pytest.mark.parametrize('check_file',[(True,True)])
+# @pytest.mark.parametrize(
+#     'temp_config',
+#     [("config.json","BTC.csv"),],
+#     indirect=True
+#     )
+def test():
+    pass
+
+scenarios=[
+    (("config.json",),False,()),
+    (("config.json",),True,()),
+    (("config.json",),True,("BTC.csv",))
+]
+
+
 @pytest.mark.parametrize(
-    'temp_test_env',
-    [("config.json",),],
-    indirect=True
+    "temp_config,run,temp_data",
+    scenarios,
+    indirect=["temp_config","temp_data"]
     )
-@pytest.mark.wip
-def test_load_config(
-    temp_test_env,
-    check_file,run
-    ):
-    root=temp_test_env
+def test_overall_run(temp_config,run,temp_data):
+    root=temp_config
+    temp_data
     c=config(str(root/"config"))
     s=Scraper(c)
     assert isinstance(s.config,config)
     if run:
         s.run()
-    if check_file:
         assert os.path.isfile(root/"data"/"ts"/"BTC.csv")
+        df=pd.read_csv(root/"data"/"ts"/"BTC.csv")
+        df["Date"]=pd.to_datetime(df["Date"])
+        assert not any(df["Date"].duplicated())
+        assert df["Date"].iat[-1].date()==datetime.date.today()+datetime.timedelta(-1)
