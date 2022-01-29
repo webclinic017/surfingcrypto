40beba8f9d25e2fcc20a7747ab2b149d800b531c
giocaizzi <giocaizzi@gmail.com>
2022-01-29 18:15:27 +0100

    telegram testing

--- tests/test_telegram_bot.py
+++ tests/test_telegram_bot.py
@@ -1,42 +1,19 @@
-import unittest 
-from surfingcrypto.telegram_bot import Tg_notifications 
-from surfingcrypto.config import config
+"""
+test telegram bot
+"""
 import pandas as pd
 import time
 import pytest
+from surfingcrypto.telegram_bot import Tg_notifications 
+from surfingcrypto.config import config
 
-@pytest.mark.skip
-class TestTelegramBot(unittest.TestCase):
-
-    def setUp(self):
-        parent="/Users/giorgiocaizzi/Documents/GitHub/surfingcrypto/"
-        self.c=config(parent+"config",parent+"data")
-        self.tg=Tg_notifications(self.c)
-    
-    def test_loading_config(self):
-        self.assertIsInstance(self.tg.token,str)
-
-    def test_mode(self):
-        self.assertIs(self.tg.channel_mode,False)
-    
-
-@pytest.mark.skip
-class TestTelegramBotChannelMode(unittest.TestCase):
-
-    def setUp(self):
-        parent="/Users/giorgiocaizzi/Documents/GitHub/surfingcrypto/"
-        self.c=config(parent+"config",parent+"data")
-        self.tg=Tg_notifications(self.c,channel_mode=True)
-    
-    def test_loading_config(self):
-        self.assertIsInstance(self.tg.token,str) 
-
-    def test_mode(self):
-        self.assertIs(self.tg.channel_mode,True)   
-
-    def test_loading_users(self):
-        self.assertIsInstance(self.tg.users,pd.DataFrame)  
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize(
+    "temp_test_env",
+    [("config.json",)],
+    indirect=["temp_test_env"]
+    )
+def test_missing_configuration(temp_test_env):
+    root=temp_test_env
+    c=config(str(root/"config"))
+    with pytest.raises(ValueError):
+        assert Tg_notifications(c)
