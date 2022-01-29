b6727f3c01896168f81e86e864052076b434e550
giocaizzi <giocaizzi@gmail.com>
2022-01-29 18:16:35 +0100

    test ts

--- tests/test_ts.py
+++ tests/test_ts.py
@@ -17,13 +17,3 @@ def test_failed_load_data(temp_test_env):
     c=config(str(root/"config"))
     with pytest.raises(FileNotFoundError):
         assert TS(c,coin="BTC")
-
-@pytest.mark.parametrize(
-    "temp_test_env",
-    [(("config.json",),("BTC.csv",))],
-    indirect=["temp_test_env"]
-    )
-def test_failed_load_data(temp_test_env):
-    root=temp_test_env
-    c=config(str(root/"config"))
-    ts=TS(c,coin="BTC")
