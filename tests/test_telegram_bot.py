8712d68c13e9329d34e96aa1d5262b0639fa60ad
giocaizzi <giocaizzi@gmail.com>
2022-02-08 22:10:08 +0100

    just another secret

--- tests/test_telegram_bot.py
+++ tests/test_telegram_bot.py
@@ -3,17 +3,40 @@ test telegram bot
 """
 import pytest
 import asyncio
+import pytest_asyncio
 import telegram
-from telethon import TelegramClient,events
+from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import decouple
 import pandas as pd
-import pytest_asyncio
-import time
-import random,string
+import random, string
 
 from surfingcrypto.telegram_bot import Tg_notifications
 from surfingcrypto import Config
+
+API_ID = decouple.config("TELEGRAM_API_ID")
+API_HASH = decouple.config("TELEGRAM_API_HASH")
+PASSWORD = decouple.config("TELEGRAM_2FA_PASSWORD")
+STRINGSESSION = decouple.config("TELETHON_STRINGSESSION")
+USER_ID = decouple.config("TELEGRAM_USER_ID")
+
+
+@pytest.fixture
+async def telegram_user(request):
+    """user to interact with bot programmatically"""
+    client = TelegramClient(StringSession(STRINGSESSION), API_ID, API_HASH)
+
+    async def init():
+        await client.connect()
+        await client.sign_in(password=PASSWORD)
+        return client
+
+    yield await init()
+
+    def finalizer():
+        client.disconnect()
+
+    request.addfinalizer(finalizer)
 
 
 @pytest.mark.parametrize(
@@ -26,6 +49,7 @@ def test_missing_configuration(temp_test_env):
     with pytest.raises(ValueError):
         assert Tg_notifications(c)
 
+
 @pytest.mark.parametrize(
     "temp_test_env", [("config_telegram.json",)], indirect=["temp_test_env"]
 )
@@ -37,74 +61,72 @@ def test_init_testbot(temp_test_env):
     assert isinstance(t.configuration, Config)
     assert t.token == c.telegram["token"]
     assert t.channel_mode is False
-    assert isinstance(t.bot,telegram.Bot)
-    isinstance(t.updates,pd.DataFrame)
+    assert isinstance(t.bot, telegram.Bot)
+    isinstance(t.updates, pd.DataFrame)
 
 
-API_ID=decouple.config("TELEGRAM_API_ID")
-API_HASH=decouple.config("TELEGRAM_API_HASH")
-PASSWORD=decouple.config("TELEGRAM_2FA_PASSWORD")
-STRINGSESSION=decouple.config("TELETHON_STRINGSESSION")
-
-@pytest.fixture
-async def telegram_user(request):
-    client=TelegramClient(StringSession(STRINGSESSION), API_ID, API_HASH)
-    async def init():
-        await client.connect() 
-        await client.sign_in(password=PASSWORD)
-        return client
-    yield await init()
-    def finalizer():
-        client.disconnect()
-    request.addfinalizer(finalizer)
-
-@pytest.mark.wip
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "temp_test_env", [("config_telegram.json",)], indirect=["temp_test_env"]
 )
-async def test_init_testbot_getting_updates(temp_test_env,telegram_user):
+async def test_init_testbot_and_get_updates(temp_test_env, telegram_user):
     """send unique text to bot and process updates correctly"""
     root = temp_test_env
     c = Config(str(root / "config"))
-    client=telegram_user
+    # fixture
+    client = telegram_user
 
-    # This part is IMPORTANT, because it fills the entity cache.
+    # it fills the entity cache.
     dialogs = await client.get_dialogs()
-    entity=await client.get_entity('surfingcrypto_test_bot')
+    entity = await client.get_entity("surfingcrypto_test_bot")
 
-    unique_test_message=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    await client.send_message(entity=entity,message=unique_test_message)
+    unique_test_message = "".join(
+        random.choice(string.ascii_uppercase + string.digits) for _ in range(10)
+    )
+    await client.send_message(entity=entity, message=unique_test_message)
 
     t = Tg_notifications(c)
     assert len(t.updates) > 0
     assert unique_test_message in t.updates["message"].tolist()
 
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "temp_test_env", [("config_telegram.json",)], indirect=["temp_test_env"]
+)
+async def test_send_message(temp_test_env, telegram_user):
+    """send unique text to bot and process updates correctly"""
+    root = temp_test_env
+    c = Config(str(root / "config"))
+    client = telegram_user
+    t = Tg_notifications(c)
+
+    # fetch id of user interacting with bot
+    async for dialog in client.iter_dialogs():
+        if dialog.name == "surfingcrypto_testbot":
+            entity = dialog.entity
+            # chat_id = dialog.message.from_id.user_id #randomdly resitutes attribute error
+            break
+
+    unique_test_message = "".join(
+        random.choice(string.ascii_uppercase + string.digits) for _ in range(10)
+    )
+    t.send_message(unique_test_message, USER_ID)
+
+    async for message in client.iter_messages(entity):
+        if str(message.raw_text) == unique_test_message:
+            found = True
+    assert found is True
+
+
 @pytest.mark.wip
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "temp_test_env", [("config_telegram.json",)], indirect=["temp_test_env"]
 )
-async def test_send_message(temp_test_env,telegram_user):
+async def test_send_message(temp_test_env, telegram_user):
     """send unique text to bot and process updates correctly"""
     root = temp_test_env
     c = Config(str(root / "config"))
-    client=telegram_user
+    client = telegram_user
     t = Tg_notifications(c)
-
-    async for dialog in client.iter_dialogs():
-        if dialog.name=="surfingcrypto_testbot":
-            entity=dialog.entity
-            chat_id = dialog.message.from_id.user_id
-            break
-
-    unique_test_message=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    t.send_message(unique_test_message,chat_id)
-    async for message in client.iter_messages(entity):
-        if str(message.raw_text)==unique_test_message:
-            found=True
-    assert found is True
-
-
-
-
