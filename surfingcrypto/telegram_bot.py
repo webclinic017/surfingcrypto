a0bf9244d7652c7afe0c7e80d6c78674ad31df31
giocaizzi <giocaizzi@gmail.com>
2022-02-26 17:33:09 +0100

    format

--- surfingcrypto/telegram_bot.py
+++ surfingcrypto/telegram_bot.py
@@ -5,12 +5,13 @@ import telegram
 import os
 import pandas as pd
 
+
 class Tg_notifications:
     """
     Wrapper of official telegram API to send notifications from a BOT initiaed manually with @BotFather.
 
     Arguments:
-		configuration (:obj:`surfingcrypto.config.config`) : package configuration object
+        configuration (:obj:`surfingcrypto.config.config`) : package configuration object
         channel_mode (bool) : init class in channel_bot, send to all chat_id contained in telegram_users.csv
         new_users_check (bool):  check for new users and add new ones to telegram_users.csv
     
@@ -22,36 +23,42 @@ class Tg_notifications:
     
     """
 
-    def __init__(self,
-        configuration,
-        channel_mode=False,
-        new_users_check=True,
-        ):
+    def __init__(
+        self, configuration, channel_mode=False, new_users_check=True,
+    ):
 
-        self.error_log=[]
+        self.error_log = []
 
-        if hasattr(configuration,"telegram"):
-            
-            self.configuration=configuration
+        if hasattr(configuration, "telegram"):
 
-            self.channel_mode=channel_mode
+            self.configuration = configuration
+
+            self.channel_mode = channel_mode
             self.token = self.configuration.telegram["token"]
-            
-            #init official bot
-            self.bot=telegram.Bot(token=self.token)
+
+            # init official bot
+            self.bot = telegram.Bot(token=self.token)
             self.getUpdates()
 
             if self.channel_mode:
-                if os.path.isfile(self.configuration.config_folder+"/telegram_users.csv"):
-                    self.users=pd.read_csv(self.configuration.config_folder+"/telegram_users.csv")
-                    self.users["date_joined"]=pd.to_datetime(self.users["date_joined"])
+                if os.path.isfile(
+                    self.configuration.config_folder + "/telegram_users.csv"
+                ):
+                    self.users = pd.read_csv(
+                        self.configuration.config_folder + "/telegram_users.csv"
+                    )
+                    self.users["date_joined"] = pd.to_datetime(
+                        self.users["date_joined"]
+                    )
                     if new_users_check:
                         print("### TELEGRAM BOT")
                         print("# Checking new users")
                         self.new_users()
                 else:
-                    raise FileNotFoundError("config folder contain a csv file containing usernames and chat IDs.")        
-            
+                    raise FileNotFoundError(
+                        "config folder contain a csv file containing usernames and chat IDs."
+                    )
+
         else:
             raise ValueError("config.json file must contain a telegram bot token.")
 
@@ -59,15 +66,15 @@ class Tg_notifications:
         """
         get updates from bot
         """
-        self.updates=[]
+        self.updates = []
         for update in self.bot.getUpdates(timeout=2):
-            i={}
-            i["username"]=update.message.from_user.username
-            i["date"]=update.message.date
-            i["chat_id"]=update.message.chat_id
-            i["message"]=update.message.text
+            i = {}
+            i["username"] = update.message.from_user.username
+            i["date"] = update.message.date
+            i["chat_id"] = update.message.chat_id
+            i["message"] = update.message.text
             self.updates.append(i)
-        self.updates=pd.DataFrame(self.updates)
+        self.updates = pd.DataFrame(self.updates)
         return self.updates
 
     def new_users(self):
@@ -75,23 +82,28 @@ class Tg_notifications:
         check if unknown users have interacted with the telegram bot.
         If the new users are found, they are automatically added to known users csv files.
         """
-        if len(self.updates)>=1:
-            updates=self.updates.set_index("chat_id")[["username","date"]].drop_duplicates()
-            
-            new_users_mask=~updates.index.isin(self.users["chat_id"])
+        if len(self.updates) >= 1:
+            updates = self.updates.set_index("chat_id")[
+                ["username", "date"]
+            ].drop_duplicates()
+
+            new_users_mask = ~updates.index.isin(self.users["chat_id"])
             if any(new_users_mask):
-                new_users=updates[new_users_mask].reset_index()
-                new_users.rename({"date":"date_joined"},axis=1,inplace=True)
-                self.users=self.users.append(new_users,ignore_index=True)
+                new_users = updates[new_users_mask].reset_index()
+                new_users.rename({"date": "date_joined"}, axis=1, inplace=True)
+                self.users = self.users.append(new_users, ignore_index=True)
                 self.users.sort_index(inplace=True)
-                self.users.to_csv(self.configuration.config_folder+"/telegram_users.csv",index=False)
+                self.users.to_csv(
+                    self.configuration.config_folder + "/telegram_users.csv",
+                    index=False,
+                )
                 print("Users successfully added!")
             else:
                 print("No new users.")
         else:
             print("No updates.")
 
-    def send_message_to_all(self,message):
+    def send_message_to_all(self, message):
         """
         send message to all known users
         
@@ -100,11 +112,13 @@ class Tg_notifications:
         """
         if self.channel_mode:
             for chat_id in self.users["chat_id"].tolist():
-                self.send_message(message=message,chat_id=chat_id)
+                self.send_message(message=message, chat_id=chat_id)
         else:
             raise ValueError("This method is available only in channel mode.")
 
-    def send_message(self,message,chat_id,):
+    def send_message(
+        self, message, chat_id,
+    ):
         """
         send message to a specific user
 
@@ -116,20 +130,16 @@ class Tg_notifications:
             self.bot.sendMessage(chat_id=chat_id, text=message)
         except Exception as e:
             print("SendMessageError")
-            self.error_log.append({
+            self.error_log.append({"error": "SendMessageError", "e": e})
 
-                "error":"SendMessageError",
-                "e":e
-            })
-
-    def send_doc(self,document,chat_id,caption=""):
+    def send_doc(self, document, chat_id, caption=""):
         """
         send document.
         """
-        with open(document,"rb") as d:
-            self.bot.send_document(chat_id=chat_id,document=d,caption=caption)
+        with open(document, "rb") as d:
+            self.bot.send_document(chat_id=chat_id, document=d, caption=caption)
 
-    def send_photo_to_all(self,photo):
+    def send_photo_to_all(self, photo):
         """
         send message to all known users
         
@@ -138,11 +148,11 @@ class Tg_notifications:
         """
         if self.channel_mode:
             for chat_id in self.users["chat_id"].tolist():
-                self.send_photo(photo=photo,chat_id=chat_id)
+                self.send_photo(photo=photo, chat_id=chat_id)
         else:
             raise ValueError("This method is available only in channel mode.")
 
-    def send_photo(self,photo,chat_id,caption=""):
+    def send_photo(self, photo, chat_id, caption=""):
         """
         send photo.
     
@@ -152,12 +162,9 @@ class Tg_notifications:
 
         """
         try:
-            with open(photo,"rb") as p:
-                self.bot.send_photo(chat_id=chat_id,photo=p,caption=caption)
+            with open(photo, "rb") as p:
+                self.bot.send_photo(chat_id=chat_id, photo=p, caption=caption)
         except Exception as e:
             print("SendPhotoError")
-            self.error_log.append({
+            self.error_log.append({"error": "SendPhotoError", "e": e})
 
-                "error":"SendPhotoError",
-                "e":e
-            })
