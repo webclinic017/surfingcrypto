0b3ac3bd715642405c7cb196ec8a86bd70a3e917
giocaizzi <giocaizzi@gmail.com>
2022-04-24 20:51:43 +0200

    before sizer

--- surfingcrypto/algotrading/strategies.py
+++ surfingcrypto/algotrading/strategies.py
@@ -40,25 +40,21 @@ class MLStrategy(bt.Strategy):
         # report executed order
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log(f"Open: {self.data_open[0]}, Close: {self.data_close[0]}")
                 self.log(
                     f"BUY EXECUTED --- Price: {order.executed.price:.2f}, Cost: {order.executed.value:.2f},Commission: {order.executed.comm:.2f}"
                 )
                 self.price = order.executed.price
                 self.comm = order.executed.comm
             else:
-                self.log(f"Open: {self.data_open[0]}, Close: {self.data_close[0]}")
                 self.log(
                     f"SELL EXECUTED --- Price: {order.executed.price:.2f}, Cost: {order.executed.value:.2f},Commission: {order.executed.comm:.2f}"
                 )
-
-        # report failed order
         elif order.status in [order.Canceled]:
             self.log(f"Order Canceled")
         elif order.status in [order.Rejected]:
             self.log(f"Order Rejected")
         elif order.status in [order.Margin]:
-            self.log(f"Order Failed: Margin")
+            self.log(f"Order Failed: Margin --- Price: {order.created.price:.3f}")
 
         # set no pending order
         self.order = None
@@ -66,12 +62,13 @@ class MLStrategy(bt.Strategy):
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
-        if self.params.verbose:
-            self.log(
-                f"OPERATION RESULT --- Gross: {trade.pnl:.2f}, Net: {trade.pnlcomm:.2f}"
-            )
+        self.log(
+            f"OPERATION RESULT --- Gross: {trade.pnl:.2f}, Net: {trade.pnlcomm:.2f}"
+        )
 
     def next(self):
+        # self.log("------")
+        # self.log(f"Open: {self.data_open[0]}, Close: {self.data_close[0]}")
         # Check if we are in the market
         if not self.position:
             if self.data_predicted > 0:
@@ -82,7 +79,7 @@ class MLStrategy(bt.Strategy):
                 )  #  fractional
                 # buy order
                 self.log(
-                    f"BUY CREATED --- Size: {size}, Cash: {self.broker.getcash():.2f}, Open: {self.data_open[0]}, Close: {self.data_close[0]}"
+                    f"BUY CREATED --- Size: {size}, Cost: {self.broker.getcash():.2f}"
                 )
                 self.buy(size=size)
         else:
