84110745eb984e7514eba76e135c4dfd8bd59ef7
giocaizzi <giocaizzi@gmail.com>
2022-09-24 19:25:24 +0200

    skip tests

--- tests/test_reporting.py
+++ tests/test_reporting.py
@@ -6,7 +6,7 @@ import pandas as pd
 from surfingcrypto.reporting.reporting import percentage_diff
 
 
-@pytest.mark.wip
+@pytest.mark.skip
 def test_percentage_diff():
     df=pd.read_csv("tests/fixtures/BTC_EUR.csv")
     assert percentage_diff(df, window=1) == (41659.188229395666 - 40925.54103342952)
